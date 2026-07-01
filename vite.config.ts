e225c1feb4fc511ad187ee128a51d8eeff757b3a
caonguyen210206-sys <caonguyen210206@gmail.com>
2026-07-01 14:59:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/emUyen/' ,
+   base: '/emUyen/',
+server: {
+port: 3000,
+host: '0.0.0.0',
+},
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
